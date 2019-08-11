9acb159b9a089a7cf2a58ca43b2b003ec3a587f2
LiTO773 <devluisp@hotmail.com>
2019-08-11 19:56:21 +0100

    Removed Moodle connection from MoodleInfoController

--- src/main/java/controllers/MoodleInfoController.java
+++ src/main/java/controllers/MoodleInfoController.java
@@ -23,8 +23,6 @@ public class MoodleInfoController {
     public PasswordField passwordField;
     public CheckBox nameCheckbox;
 
-    public JsonObject jsonObject;
-
     public void doLogin(ActionEvent actionEvent) {
         // Check if all the parameters where filled
         boolean empty = moodleURLField.getText().trim().isEmpty() ||
@@ -36,8 +34,8 @@ public class MoodleInfoController {
             return;
         }
 
-        // Create a correct URL
-        String moodleURL = null;
+        // Create a correct URL (add https:// or http:// if necessary)
+        String moodleURL;
         try {
             moodleURL = MoodleWSConnection.httpsURL(moodleURLField.getText());
         } catch (CustomException e) {
@@ -46,8 +44,11 @@ public class MoodleInfoController {
             return;
         }
 
+        // Save the arguments
+        CurrentMoodle.setParams(moodleURL, nameField.getText(), passwordField.getText());
+
         // Check if there is already an instance in the DB
-        DataDB db = null;
+        DataDB db;
         try {
             db = new DataDB();
             if (db.moodleExists(moodleURL, nameField.getText().trim())) {
@@ -60,80 +61,12 @@ public class MoodleInfoController {
             return;
         }
 
-        // Send a request to Moodle
-        TokenID tokenID = null;
-        String siteName = null;
-        try {
-            tokenID = sendLoginRequest(
-                moodleURL,
-                nameField.getText(),
-                passwordField.getText()
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-            MessageDialog.errorDialog(e.getMessage());
-            return;
-        }
-
-        // Everything went correctly, save the Moodle in CurrentMoodle
-        String name;
-        if(nameCheckbox.isSelected()){
-            name = jsonObject.get("sitename").toString();
-            name = name.substring(1, name.length() - 1);
-        } else {
-            name = moodleName.getText();
-        }
-
-        Moodle moodle = new Moodle(
-                name,
-                moodleURL,
-                nameField.getText(),
-                tokenID.getToken(),
-                tokenID.getUserid()
-        );
-        MessageDialog.infoDialog("The Moodle you inserted is valid 🎉. Please continue the setup.");
-        CurrentMoodle.setMoodle(moodle);
+        // Move to the Loading Scene
         SceneChanger sc = new SceneChanger(actionEvent);
-        sc.changeScene("MoodleActions/ChooseDirectory.fxml");
+        sc.changeScene("MoodleActions/LoadingInfo.fxml");
     }
 
-    private TokenID sendLoginRequest(String moodleURL, String username, String password) throws CustomException, IOException {
-        MoodleWSConnection moodleWS = new MoodleWSConnection();
-        TokenID tokenID = new TokenID();
 
-        // Gets the user's token (as a response)
-        String tokenResponse = moodleWS.getTokenResponse(moodleURL, username, password);
-
-        // Check if the response contains the user's token:
-        JsonObject jsonResponse = new JsonParser().parse(tokenResponse).getAsJsonObject();
-        if (jsonResponse.has("errorcode")) {
-            // An error happened, find out which one and report to the user
-            String error = jsonResponse.get("errorcode").toString();
-            switch (error) {
-                case "enablewsdescription":
-                    throw new CustomException(Errors.INCOMPATIBLE_MOODLE);
-                case "missingparam":
-                    throw new CustomException(Errors.MISSING_PARAMS);
-                case "invalidlogin":
-                    throw new CustomException(Errors.INVALID_CREDENTIALS);
-                default:
-                    throw new CustomException("Moodle returned this error: " + error);
-            }
-        }
-
-        // Everything went correctly
-        String tokenRaw = jsonResponse.get("token").toString();
-        String token = tokenRaw.substring(1, tokenRaw.length() - 1); // tokenRaw has extra ""
-        tokenID.setToken(token);
-
-        System.out.println(token);
-
-        // Get the userid
-        jsonObject = moodleWS.getUserID(moodleURL, token);
-        tokenID.setUserid(Integer.parseInt(jsonObject.get("userid").toString()));
-
-        return tokenID;
-    }
 
     public void checkCheckbox(ActionEvent actionEvent) {
         if(nameCheckbox.isSelected()){
--- src/main/java/models/CurrentMoodle.java
+++ src/main/java/models/CurrentMoodle.java
@@ -4,6 +4,30 @@ package models;
  * Static class that stores the currently used Moodle
  */
 public class CurrentMoodle {
+    // Used for the first scene and loading scene
+    private static String url;
+    private static String username;
+    private static String password;
+
+    public static void setParams(String url, String username, String password) {
+        CurrentMoodle.url = url;
+        CurrentMoodle.username = username;
+        CurrentMoodle.password = password;
+    }
+
+    public static String getUrl() {
+        return url == null ? "" : url;
+    }
+
+    public static String getUsername() {
+        return username == null ? "" : username;
+    }
+
+    public static String getPassword() {
+        return password == null ? "" : password;
+    }
+
+    // Used to store all the data structures necessary
     private static Moodle current;
 
     public static void clear() {
